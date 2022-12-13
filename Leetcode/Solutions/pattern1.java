70d375fed89a7aa73f062f79403f3e9719ff65ff
Srishti Kashyap <56797284+srish-ravenclaw@users.noreply.github.com>
2022-12-13 17:28:12 +0530

    Create pattern1.java

---
+++ Leetcode/Solutions/pattern1.java
@@ -0,0 +1,18 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
