60a580f48b70919e6dd5f38dd655be1448113543
Srishti Kashyap <56797284+srish-ravenclaw@users.noreply.github.com>
2022-12-13 17:29:11 +0530

    Create pattern2.java

---
+++ Leetcode/Solutions/pattern2.java
@@ -0,0 +1,18 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<=i ; j++) {
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
