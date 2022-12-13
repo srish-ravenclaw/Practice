ee40ebfdf1784768d115ad207a606c23c19edda2
Srishti Kashyap <56797284+srish-ravenclaw@users.noreply.github.com>
2022-12-13 17:30:10 +0530

    Create pattern3.java

---
+++ Leetcode/Solutions/pattern3.java
@@ -0,0 +1,18 @@
+// 1
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<=i ; j++) {
+                System.out.printf("%d ", j+1);
+            }
+            System.out.println("");
+        }
+    }
+}
